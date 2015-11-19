3281bcf6de489a41aa36bb0929e83a3c8616518d
dingxin <dingxin@sconit.com>
2015-11-19 14:15:45 +0800

    1

--- trunk/src/SQL Script/Parts/update sql_20151119_dx.sql
+++ trunk/src/SQL Script/Parts/update sql_20151119_dx.sql
@@ -94,3 +94,67 @@ GO
 
 alter table WMS_ShipPlan add [Priority] tinyint not null
 go
+
+/****** Object:  Table [dbo].[WMS_PickTask]    Script Date: 2015/11/19 14:15:11 ******/
+DROP TABLE [dbo].[WMS_PickTask]
+GO
+
+/****** Object:  Table [dbo].[WMS_PickTask]    Script Date: 2015/11/19 14:15:11 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[WMS_PickTask](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[OrderNo] [varchar](50) NULL,
+	[OrderSeq] [int] NULL,
+	[ShipPlanId] [int] NULL,
+	[Priority] [tinyint] NOT NULL,
+	[Item] [varchar](50) NOT NULL,
+	[ItemDesc] [varchar](100) NOT NULL,
+	[RefItemCode] [varchar](50) NULL,
+	[Uom] [varchar](5) NOT NULL,
+	[BaseUom] [varchar](5) NOT NULL,
+	[UC] [decimal](18, 8) NOT NULL,
+	[UCDesc] [varchar](50) NULL,
+	[OrderQty] [decimal](18, 8) NOT NULL,
+	[PickQty] [decimal](18, 8) NOT NULL,
+	[Loc] [varchar](50) NOT NULL,
+	[Area] [varchar](50) NULL,
+	[Bin] [varchar](50) NULL,
+	[LotNo] [varchar](50) NULL,
+	[HuId] [varchar](50) NULL,
+	[PickBy] [tinyint] NOT NULL,
+	[PickGroup] [varchar](50) NULL,
+	[PickUserId] [int] NULL,
+	[PickUserNm] [varchar](100) NULL,
+	[StartTime] [datetime] NULL,
+	[WinTime] [datetime] NOT NULL,
+	[IsActive] [bit] NOT NULL,
+	[CreateUser] [int] NOT NULL,
+	[CreateUserNm] [varchar](100) NOT NULL,
+	[CreateDate] [datetime] NOT NULL,
+	[LastModifyUser] [int] NOT NULL,
+	[LastModifyUserNm] [varchar](100) NOT NULL,
+	[LastModifyDate] [datetime] NOT NULL,
+	[CloseUser] [int] NULL,
+	[CloseUserNm] [varchar](100) NULL,
+	[CloseDate] [datetime] NULL,
+	[Version] [int] NOT NULL,
+ CONSTRAINT [PK_WMS_PickTask] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
