0d8bdd530207f1fcf7708218f5bef772ff132803
dingxin <dingxin@sconit.com>
2015-11-24 09:44:38 +0800

    1

---
+++ trunk/src/Core.5.0.0/Service/IShipPlanMgr.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using com.Sconit.Entity.ORD;
+
+namespace com.Sconit.Service
+{
+    public interface IShipPlanMgr
+    {
+        void CreateShipPlan(OrderMaster orderMaster);
+
+        void CancelShipPlan(OrderMaster orderMaster);
+
+
+    }
+}
