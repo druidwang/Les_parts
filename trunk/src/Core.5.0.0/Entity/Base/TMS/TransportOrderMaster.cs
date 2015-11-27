dd63891500b0f3071bb061793b31e85857fa07c4
dingxin <dingxin@sconit.com>
2015-11-27 15:09:01 +0800

    1

--- trunk/src/Core.5.0.0/Entity/Base/TMS/TransportOrderMaster.cs
+++ trunk/src/Core.5.0.0/Entity/Base/TMS/TransportOrderMaster.cs
@@ -55,6 +55,9 @@ namespace com.Sconit.Entity.TMS
 		public string CancelUserName { get; set; }
 		public Int32? Version { get; set; }
         public string LicenseNo { get; set; }
+        public Int32? CurrentArriveSiteId { get; set; }
+        public string CurrentArriveShipAddress { get; set; }
+        public string CurrentArriveShipAddressDescription { get; set; }
         
         #endregion
 
