20bb90dff042f4fcc192555b98e04e2cb4a37e5d
dingxin <dingxin@sconit.com>
2015-12-16 11:50:19 +0800

    1

--- trunk/src/SQL Script/Parts/stored procedures/001.USP_WMS_CreatePickTask.sql
+++ trunk/src/SQL Script/Parts/stored procedures/001.USP_WMS_CreatePickTask.sql
@@ -11,6 +11,7 @@ GO
 
 CREATE PROCEDURE dbo.USP_WMS_CreatePickTask
 (
+	@CreatePickTaskTable CreatePickTaskTableType readonly,
 	@CreateUserId int,
 	@CreateUserNm varchar(100)
 )
@@ -19,8 +20,77 @@ BEGIN
 	set nocount on
 	declare @DateTimeNow datetime
 	declare @ErrorMsg nvarchar(MAX)
-	declare @IsPickHu bit
-
 	
+	create table #tempMsg_001 
+	(
+		Id int identity(1, 1) primary key,
+		Lvl tinyint,
+		Msg varchar(2000)
+	)
+
+	create table #tempShipPlan_001
+	(
+		ShipPlanId int primary key,
+		OrderQty decimal(18, 8),
+		PickQty decimal(18, 8),
+		ThisPickQty decimal(18, 8),
+		IsShipScanHu bit,
+		PickBy tinyint,
+		IsActive bit,
+	)
+
+	begin try
+
+		insert into #tempShipPlan_001(ShipPlanId, OrderQty, PickQty, ThisPickQty, IsShipScanHu, PickBy, IsActive)
+		select sp.Id, sp.OrderQty, sp.PickQty, tmp.PickQty, sp.IsShipScanHu, l.PickBy, sp.IsActive
+		from @CreatePickTaskTable as tmp 
+		inner join WMS_ShipPlan as sp on tmp.Id = sp.Id
+		inner join MD_Location as l on sp.LocFrom = l.Code
+
+		--检查发运计划是否关闭
+		insert into #tempMsg_001(Lvl, Msg)
+		select 2, N'发货任务'+ convert(varchar, ShipPlanId) + N'已经关闭。'
+		from #tempShipPlan_001 where IsActive = 0
+
+		--检查创建的拣货单数量是否超过了计划数
+		insert into #tempMsg_001(Lvl, Msg)
+		select 2, N'发货任务'+ convert(varchar, ShipPlanId) + N'的拣货数已经超过了订单数。'
+		from #tempShipPlan_001 where IsActive = 1
+		and PickQty + ThisPickQty > OrderQty
+
+		if exists(select top 1 1 from #tempMsg_001)
+		begin
+			declare @CreatePickTask4PickQtyTable CreatePickTaskTableType
+			declare @CreatePickTask4PickLotNoTable CreatePickTaskTableType
+			declare @CreatePickTask4PickHuTable CreatePickTaskTableType
+
+			insert into @CreatePickTask4PickQtyTable(Id, PickQty) select ShipPlanId, ThisPickQty from #tempShipPlan_001 where IsShipScanHu = 0
+			insert into @CreatePickTask4PickLotNoTable(Id, PickQty) select ShipPlanId, ThisPickQty from #tempShipPlan_001 where IsShipScanHu = 1 and PickBy = 0
+			insert into @CreatePickTask4PickHuTable(Id, PickQty) select ShipPlanId, ThisPickQty from #tempShipPlan_001 where IsShipScanHu = 1 and PickBy = 1
+
+			if exists(select top 1 1 from @CreatePickTask4PickQtyTable)
+			begin
+				exec USP_WMS_CreatePickTask4PickQty @CreatePickTask4PickQtyTable, @CreateUserId,@CreateUserNm 
+			end
+
+			if exists(select top 1 1 from @CreatePickTask4PickLotNoTable)
+			begin
+				exec USP_WMS_CreatePickTask4PickLotNo @CreatePickTask4PickLotNoTable, @CreateUserId,@CreateUserNm 
+			end
+
+			if exists(select top 1 1 from @CreatePickTask4PickHuTable)
+			begin
+				exec USP_WMS_CreatePickTask4PickHu @CreatePickTask4PickHuTable, @CreateUserId,@CreateUserNm 
+			end
+		end
+	end try
+	begin catch
+		set @ErrorMsg = N'创建拣货任务发生异常：' + Error_Message() 
+		RAISERROR(@ErrorMsg, 16, 1) 
+	end catch
+
+	select Lvl, Msg from #tempMsg_001 order by Id
+
+	drop table #tempMsg_001
 END
 GO
---
+++ trunk/src/SQL Script/Parts/stored procedures/003.USP_WMS_GetAvailableInv4Pick.sql
@@ -0,0 +1,114 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF EXISTS(SELECT * FROM SYS.objects WHERE type='P' AND name='USP_WMS_GetAvailableInv4Pick')
+BEGIN
+	DROP PROCEDURE USP_WMS_GetAvailableInv4Pick
+END
+GO
+
+CREATE PROCEDURE dbo.USP_WMS_GetAvailableInv4Pick
+	@PickTargetTable PickTargetTableType readonly
+AS
+BEGIN
+	set nocount on
+	declare @ErrorMsg nvarchar(MAX)
+
+	create table #tempPickTarget_003
+	(
+		RowId int identity(1, 1),
+		Location varchar(50),
+		Item varchar(50)
+	)
+
+	create table #tempLocation_003
+	(
+		RowId int identity(1, 1),
+		Location varchar(50),
+		Suffix varchar(50)
+	)
+
+	begin try
+		if not exists(select top 1 1 FROM tempdb.sys.objects WHERE type = 'U' AND name like '#tempAvailableInv_003%') 
+		begin
+			set @ErrorMsg = '没有定义返回数据的参数表。'
+			RAISERROR(@ErrorMsg, 16, 1) 
+
+			--代码不会执行到这里
+			create table #tempAvailableInv_003
+			(
+				RowId int identity(1, 1),
+				Location varchar(50),
+				Item varchar(50),
+				Qty decimal(18, 8)
+			)
+		end
+		else
+		begin
+			truncate table #tempAvailableInv_003
+		end
+
+		insert into #tempPickTarget_003(Location, Item) select distinct Location, Item from @PickTargetTable
+		insert into #tempLocation_003(Location, Suffix) 
+		select distinct l.Code, l.PartSuffix
+		from #tempPickTarget_003 as tmp inner join MD_Location as l on tmp.Location = l.Code
+
+		declare @LocationRowId int
+		declare @MaxLocationRowId int
+		declare @Location varchar(50)
+		declare @IsSpread bit
+		declare @PickBy tinyint
+		declare @LocSuffix varchar(50)
+		declare @SelectInvStatement varchar(max)
+
+		select @LocationRowId = MIN(RowId), @MaxLocationRowId = MAX(RowId) from #tempLocation_003
+
+		while (@LocationRowId <= @MaxLocationRowId)
+		begin
+			select @Location = Location, @LocSuffix = Suffix
+			from #tempLocation_003 where RowId = @LocationRowId
+			set @SelectInvStatement = ''
+
+			set @SelectInvStatement = 'select sp.Location, sp.Item, SUM(ISNULL(llt.Qty, 0)) as InvQty '
+			set @SelectInvStatement = @SelectInvStatement + 'from #tempPickTarget_003 as sp '
+			set @SelectInvStatement = @SelectInvStatement + 'inner join INV_LocationLotDet_' + @LocSuffix + ' as llt on sp.Location = llt.Location and sp.Item = llt.Item '
+			set @SelectInvStatement = @SelectInvStatement + 'where sp.Location = ' + @Location + ' and llt.OccupyRefNo is null and llt.Qty <> 0 and llt.HuId is null and llt.QualityType = 0'
+			set @SelectInvStatement = @SelectInvStatement + 'group by sp.Location, sp.Item '
+			set @SelectInvStatement = @SelectInvStatement + 'having SUM(llt.Qty, 0) > 0'
+
+			insert into #tempAvailableInv_003(Location, Item, Qty) exec sp_executesql @SelectInvStatement
+
+			set @LocationRowId = @LocationRowId + 1
+		end
+
+		--扣减被占用的库存
+		update inv set Qty = inv.Qty - oc.OccupyQty
+		from #tempAvailableInv_003 as inv inner join
+		(select ai.Location, ai.Item, SUM(OccupyQty) as OccupyQty
+		from (select sp.Location, sp.Item, SUM(bi.Qty) as OccupyQty 
+				from #tempPickTarget_003 as sp
+				inner join WMS_BuffInv as bi on bi.Loc = sp.Location and bi.Item = sp.Item
+				where bi.Qty > 0 and bi.HuId is null and bi.IOType = 1
+				group by sp.Location, sp.Item
+				union all 
+				select pt.Loc, pt.Item, SUM((pt.OrderQty - pt.PickQty) * pt.UnitQty) as OccupyQty
+				from #tempPickTarget_003 as sp 
+				inner join WMS_PickTask as pt on sp.Location = pt.Loc and sp.Item = pt.Item
+				where pt.IsPickHu = 0 and pt.IsActive = 1 and pt.OrderQty > pt.PickQty
+				group by pt.Loc, pt.Item) as ai
+		group by ai.Location, ai.Item) as oc on inv.Location = oc.Location and inv.Item = oc.Item
+
+		--删除数量小于等于0的库存
+		delete from #tempAvailableInv_003 where Qty <= 0
+	end try
+	begin catch
+		set @ErrorMsg = N'获取拣货可用库存发生异常：' + Error_Message() 
+		RAISERROR(@ErrorMsg, 16, 1) 
+	end catch
+
+	drop table #tempLocation_003
+	drop table #tempPickTarget_003
+END
+GO
